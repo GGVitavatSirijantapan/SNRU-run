395075d7e054052e6bec451e551c8f5968cda843
vitavat <vitavat126@gmail.com>
2016-05-15 23:33:53 -0700

    clickเชื่อมไปอีกหน้า

--- app/src/main/java/snru/sirijantapan/vitavat/snrurun/MainActivity.java
+++ app/src/main/java/snru/sirijantapan/vitavat/snrurun/MainActivity.java
@@ -1,7 +1,9 @@
 package snru.sirijantapan.vitavat.snrurun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }   // main Method
+
+    public void clickSingUpMain(View view) {
+        startActivity(new Intent(MainActivity.this, SingUp.class));
     }
-}
+
+}   //main class นี้คือ คลาสหลัก
